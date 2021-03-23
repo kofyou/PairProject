128146bc2052bf5fad8e0f5ae4838f3a24653e08
Fzuerzmj <794858610@qq.com>
2021-03-23 22:14:07 +0800

    1222:创建vue框架及添加element ui和axios模块
    1222:创建vue框架及添加element ui和axios模块

---
+++ 221801209&221801222/frontend/itemname/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+      <!-- 路由占位符 -->
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+
+  components:{
+  },
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ 221801209&221801222/frontend/itemname/src/components/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class = "login-view">
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+//scoped只影响本组件
+<style scoped>
+.login-view {
+  background-image: url("../assets/background.jpg");
+  background-attachment: fixed;
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  height: 610px;
+}
+</style>
