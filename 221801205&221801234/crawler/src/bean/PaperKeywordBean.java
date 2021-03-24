f396f7f50bf5285a7523466ddcc3f6e4e0d9277d
gz321 <2582086674@qq.com>
2021-03-24 14:59:45 +0800

    编写bean类

---
+++ 221801205&221801234/crawler/src/bean/PaperKeywordBean.java
@@ -0,0 +1,20 @@
+package bean;
+
+public class PaperKeywordBean {
+	private PaperBean pb;
+	private KeywordBean kb;
+	
+	public PaperBean getPb() {
+		return pb;
+	}
+	public void setPb(PaperBean pb) {
+		this.pb = pb;
+	}
+	public KeywordBean getKb() {
+		return kb;
+	}
+	public void setKb(KeywordBean kb) {
+		this.kb = kb;
+	}
+	
+}
