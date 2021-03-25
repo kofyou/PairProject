ca111d0365f80eebbc97ce550a89210e096531ba
koopi0123 <1034088383@qq.com>
2021-03-25 18:37:38 +0800

    数据库 redis初配置

--- 221801429&221801432/paper/manage.py
+++ 221801429&221801432/paper/manage.py
@@ -4,12 +4,15 @@
 
 from flask import Flask
 from flask_sqlalchemy import  SQLAlchemy
+from redis import StrictRedis
 
 
 class Config(object):
     SQLALCHEMY_DATABASE_URL = "mysql+pymysql://%s:%s@%s/%s" % ('root', 'HRYYCFLqswslhk1', '127.0.0.1', 'paper')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
 
 
 app = Flask(__name__)
@@ -17,10 +20,14 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
+redis_store = StrictRedis(host= Config.REDIS_HOST, port= Config.REDIS_PORT, decode_responses=True)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    redis_store.set("name", "陈耀")
+    print(redis_store.get("name"))
+    print("1111")
+    return 'Hello World!'+ redis_store.set("name", "陈耀")
 
 
 if __name__ == '__main__':
