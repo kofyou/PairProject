292260335a0eb20a094e91a4006b961caa7bb315
ccrjbcai <1126774947@qq.com>
2021-03-25 21:57:58 +0800

    modify config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const config = {
-  sqlurl: "mysql://root:123456@127.0.0.1/article",
+  sqlurl: "mysql://root:123456@db/article",
   sessionSecret: "createdByCCreater-session-secret-you-will-never-guess-!!!!"
 }
 module.exports = config
