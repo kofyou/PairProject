e91e0b0237fbb3d083c5aca97787588018b2bca9
xuyaaaaaaaaaaabi <1241859128@qq.com>
2021-03-25 15:16:26 +0800

    jquery-ajax

--- 221801103&221801125/js/login.js
+++ 221801103&221801125/js/login.js
@@ -1,20 +1,26 @@
-document.getElementById("login").onclick=()=>{
-    console.log("12")
-}
-
-var inputs = document.getElementsByTagName("input");
-
-document.getElementById("login").onclick = ()=>{
-    if(inputs[0].value!="" && inputs[1].value!="")
-    {
-        //校验,登录
-        window.open("../pages/index.html","_self")
+$(function(){
+    document.getElementById("login").onclick=()=>{
+        console.log("12")
     }
-    else
-    {
-        if(inputs[0].value=="")
-            inputs[0].style.borderBottom = "3px solid #B22222"
-        if(inputs[1].value=="")
-            inputs[1].style.borderBottom = "3px solid #B22222"
+
+    var inputs = document.getElementsByTagName("input");
+
+    document.getElementById("login").onclick = ()=>{
+        if(inputs[0].value!="" && inputs[1].value!="")
+        {
+            //校验,登录
+            //window.open("../pages/index.html","_self") 
+            $("#login_word").addClass("to_none")
+            $("#reg_wait").css("display","inline-block")
+        }
+        else
+        {
+            $("#login_word").removeClass("to_none")
+            $("#reg_wait").css("display","none")
+            if(inputs[0].value=="")
+                inputs[0].style.borderBottom = "3px solid #B22222"
+            if(inputs[1].value=="")
+                inputs[1].style.borderBottom = "3px solid #B22222"
+        }
     }
-}
+})
