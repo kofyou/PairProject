ece49ed4f78cd0b116336ec22226090219d6651c
HuangJunQian <842724240@qq.com>
2021-03-26 18:37:29 +0800

    feat:新增论文业务逻辑类和收藏夹类

--- 221801226_221801232/WebContent/main.jsp
+++ 221801226_221801232/WebContent/main.jsp
@@ -2,19 +2,71 @@
 <%@ page import="entity.*"%>
 <%@ page import="servlet.*"%>
 <%@ page import="util.*"%>
-<%@ page language="java" contentType="text/html; charset=UTF-8"
+<%@ page language="java" import="java.util.*" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Paper Search For U</title>
-</head>
-<body>
-<%
-//从提供的数据地址读取数据导入数据库
-        GetDataToDB logfile = new GetDataToDB("D://ECCV");
- %>
+    <head>
+        <base href="<%=basePath%>">
 
+        <meta charset="UTF-8">
+        <meta http-equiv="pragma" content="no-cache">
+        <meta http-equiv="cache-control" content="no-cache">
+        <meta http-equiv="expires" content="0">    
+        <meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+        <meta http-equiv="description" content="This is my page">
+        <title>Paper Search For U</title>
+        
+	    <link rel="stylesheet" type="text/css" href="css/styles.css">
+	    
+    </head>
+<body>
+<!-- 读取本地数据到数据库 -->
+<%
+        //GetDataToDB logfile = new GetDataToDB("D://ECCV");
+
+ %>
+    <h1>论文库</h1>
+        <hr>
+    
+        <table width="750" height="60" cellpadding="0" cellspacing="0" border="0">
+        <tr>
+            <td>
+            
+            <!-- 论文循环开始 -->
+            <% 
+                ItemsDao itemsDao = new ItemsDao(); 
+                ArrayList<items> list = itemsDao.getAllItems();
+                //ArrayList<items> list = itemsDao.getItemsByTitle("Deep");
+                if(list!=null&&list.size()>0)
+                {
+                    for(int i=0;i<list.size();i++)
+                    {
+                        items item = list.get(i);
+            %>   
+            <div>
+                <dl>
+                <dt>
+                    <a href="details.jsp?id=<%=item.getId()%>"></a>
+                </dt>
+                <dd class="dd_title"><%=item.getTitle() %></dd> 
+                <dd class="dd_abstract">abstract:&nbsp;&nbsp;&nbsp;&nbsp;<%=item.getAbstracts() %></dd> 
+                <dd class="dd_conference">conference: &nbsp;&nbsp;<%=item.getConference() %></dd> 
+                
+                </dl>
+            </div>
+            <!-- 论文循环结束 -->
+            
+            <%
+                    }
+                } 
+            %>
+            </td>
+        </tr>
+        </table>
 </body>
 </html>
--- 221801226_221801232/src/dao/ItemsDao.java
+++ 221801226_221801232/src/dao/ItemsDao.java
@@ -1,7 +1,177 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import util.DBHelper;
+
+import entity.items;
 
 //论文的业务逻辑类
 public class ItemsDao {
 
+    /**
+     * 获取所有的论文信息
+     * @return
+     */
+    public ArrayList<items> getAllItems() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList<items> list = new ArrayList<items>(); // 论文集合
+        try {
+            conn = DBHelper.getConnection();
+            String sql = "select * from paperslist;"; // SQL语句
+            stmt = conn.prepareStatement(sql);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                items item = new items();
+                item.setId(rs.getString("id"));
+                item.setAbstracts(rs.getString("abstracts"));
+                item.setConference(rs.getString("conference"));
+                item.setKeyword(rs.getString("keyword"));
+                item.setTime(rs.getString("time"));
+                item.setTitle(rs.getString("title"));
+                item.setLink(rs.getString("link"));
+                list.add(item);// 把一个论文加入集合
+            }
+            return list; // 返回集合。
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        } finally {
+            // 释放数据集对象
+            if (rs != null) {
+                try {
+                    rs.close();
+                    rs = null;
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            // 释放语句对象
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                    stmt = null;
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 根据论文Id获取论文信息
+     * @param id
+     * @return
+     */
+    public items getItemsById(int id) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DBHelper.getConnection();
+            String sql = "select * from paperslist where id=?;"; // SQL语句
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                items item = new items();
+                item.setId(rs.getString("id"));
+                item.setAbstracts(rs.getString("abstracts"));
+                item.setConference(rs.getString("conference"));
+                item.setKeyword(rs.getString("keyword"));
+                item.setTime(rs.getString("time"));
+                item.setTitle(rs.getString("title"));
+                item.setLink(rs.getString("link"));
+                return item;
+            } else {
+                return null;
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        } finally {
+            // 释放数据集对象
+            if (rs != null) {
+                try {
+                    rs.close();
+                    rs = null;
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            // 释放语句对象
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                    stmt = null;
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+    }
+    
+    /**
+     * 根据论文标题检索论文
+     * @param title
+     * @return
+     */
+    public ArrayList<items> getItemsByTitle(String title) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList<items> list = new ArrayList<items>(); // 论文集合
+        try {
+            conn = DBHelper.getConnection();
+            String sql = "select * from paperslist where title like ?;"; // SQL语句
+            stmt = conn.prepareStatement(sql);
+            String condition = "%" + title + "%";
+            stmt.setString(1, condition);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                items item = new items();
+                item.setId(rs.getString("id"));
+                item.setAbstracts(rs.getString("abstracts"));
+                item.setConference(rs.getString("conference"));
+                item.setKeyword(rs.getString("keyword"));
+                item.setTime(rs.getString("time"));
+                item.setTitle(rs.getString("title"));
+                item.setLink(rs.getString("link"));
+                list.add(item);// 把一个论文加入集合
+                
+            }
+            return list;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        } finally {
+            // 释放数据集对象
+            if (rs != null) {
+                try {
+                    rs.close();
+                    rs = null;
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            // 释放语句对象
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                    stmt = null;
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+    }
 }
