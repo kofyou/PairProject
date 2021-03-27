28796c8b5fd9a67984cc937e50b7989cc4a138ca
FZU-CYD <1354150339@qq.com>
2021-03-27 17:46:37 +0800

    实现翻页功能的servlet

--- 221801109&221801130/src/servlet/PostServlet.java
+++ 221801109&221801130/src/servlet/PostServlet.java
@@ -23,35 +23,52 @@ import java.util.List;
 @WebServlet("/hello")
 public class PostServlet extends HttpServlet {
 
+    PostDAO postDAO = new PostDAOImpl();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
         req.setCharacterEncoding("utf-8");
         List<Post> list = new ArrayList<>() ;
-        String string = req.getParameter("string");
+        int count = 0;
+        String string = " ";
+        if (req.getParameter("count") != null) {
+            count = Integer.parseInt(req.getParameter("count"));
+            string = req.getParameter("string");
+        } else if (req.getParameter("nex") != null) {
+            count = Integer.parseInt(req.getParameter("nex")) + 1;
+            string = req.getParameter("string2");
+        } else if (req.getParameter("pre") != null) {
+            count = Integer.parseInt(req.getParameter("pre")) - 1;
+            string = req.getParameter("string1");
+        } else if (req.getParameter("string") != null){
+            string = req.getParameter("string");
+        }
         String sql = "select * from post";
         List<Post> postList = new ArrayList<>();
-       int i = 0;
-        try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement()) {
-            ResultSet rs = s.executeQuery(sql);
-            while (rs.next()) {
-                String title = rs.getString("title");
-                if (title.equals(string)){
-                    String content = rs.getString("content");
-                    String link = rs.getString("link");
-                    String keyWord = rs.getString("keyWord");
-                    String time = rs.getString("time");
-                    String platform = rs.getString("platform");
-                    postList.add(new Post(title, content, link, keyWord, time, platform));
-                }
+        postList = postDAO.list(string);
 
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        int page = 0;
+        if (postList.size() == 0){
+            req.getRequestDispatcher("/index.jsp").forward(req,resp);
         }
+        if (postList.size() % 1 == 0) {// 如果记录总条数对每页显示记录数取整等于0，则表示页面数刚好分完。
+            page = postList.size() / 1;
+        } else {// 如果取不尽，那么就添加一页来放剩余的记录
+            page = postList.size() / 1 + 1;
+        }
+        if (count < 0){
+            count = 0;
+        } else if (count > postList.size()-1) {
+            count = postList.size()-1;
+        }
+        list = postList.subList(count, count+1);
+
         req.setAttribute("key",string);
-        req.setAttribute("postList",postList);
+        req.setAttribute("postList",list);
+        req.setAttribute("count",count);
+        req.setAttribute("page",page);
         req.getRequestDispatcher("/index.jsp").forward(req,resp);
     }
+
 }
