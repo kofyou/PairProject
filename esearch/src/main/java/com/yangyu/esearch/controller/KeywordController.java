2b2f0dff55e389944f595d5816bf5d2b4d6db95a
yangyu <1849588816@qq.com>
2021-03-28 17:42:13 +0800

    新增KeywordController类

---
+++ esearch/src/main/java/com/yangyu/esearch/controller/KeywordController.java
@@ -0,0 +1,41 @@
+package com.yangyu.esearch.controller;
+
+import com.yangyu.esearch.entity.Keyword;
+import com.yangyu.esearch.service.KeywordService;
+import com.yangyu.esearch.service.PaperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author : yangyu
+ * @ClassName : com.yangyu.esearch.controller.KeywordController
+ * @Description : 类描述
+ * @date : 2021-03-28 16:29:35
+ * Copyright  2021 user. All rights reserved.
+ */
+@RestController
+@RequestMapping("/word")
+public class KeywordController {
+
+    @Autowired
+    private KeywordService keywordService;
+
+    @Autowired
+    private PaperService paperService;
+
+    @GetMapping("top")
+    public List<Keyword> topWord()
+    {
+        return keywordService.topWord(paperService.queryAll());
+    }
+
+    @GetMapping("meet")
+    public List<Keyword> meetTop(String source)
+    {
+        return keywordService.topWord(paperService.selectSource(source));
+    }
+}
