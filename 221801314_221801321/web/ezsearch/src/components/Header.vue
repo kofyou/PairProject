c6803819dddc4508dc4d969621c28c833ef21cdd
lvvvv <375042084@qq.com>
2021-03-28 15:55:05 +0800

    搜索栏调整+页头下拉栏

--- 221801314_221801321/web/ezsearch/src/components/Header.vue
+++ 221801314_221801321/web/ezsearch/src/components/Header.vue
@@ -1,74 +1,96 @@
 <template>
-    <el-row>
-        <el-col :span="4"><div class="grid-content bg-purple"><img :src="menuUrl" class="header-icon" /></div></el-col>
-        <el-col :span="16"><div class="grid-content bg-purple"><a>EzSearch</a></div></el-col>
-        <el-col :span="4">
-            <div class="grid-content bg-purple avatar">
-                <img :src="avatarUrl" class="avatar-img"/>
-            </div>
-        </el-col>
-    </el-row>
+  <el-row>
+    <el-col :span="4">
+      <div class="grid-content bg-purple">
+        <el-dropdown style="width:52px; height:52px;">
+          <span class="el-dropdown-link">
+            <i style="font-size:40px; color:#ffffff; margin-top:5px" class="el-icon-menu"></i>
+          </span>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item>热词分析</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </div>
+    </el-col>
+    <el-col :span="16"
+      ><div class="grid-content bg-purple"><a>EzSearch</a></div></el-col
+    >
+    <el-col :span="4">
+      <div class="grid-content bg-purple avatar">
+        <img :src="avatarUrl" class="avatar-img" />
+      </div>
+    </el-col>
+  </el-row>
 </template>
 <script>
-import {defineComponent} from 'vue';
-    export default defineComponent ({
-       name:'Header',
-       setup(){
-            const avatarUrl = require('@/assets/avatars/leiye.jpg');
-            const menuUrl = require('@/assets/icons/menu.png');
-            return {
-                avatarUrl,
-                menuUrl,
-           }
-       }
-    });
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Header",
+  setup() {
+    const avatarUrl = require("@/assets/avatars/leiye.jpg");
+    const menuUrl = require("@/assets/icons/menu.png");
+    return {
+      avatarUrl,
+      menuUrl,
+    };
+  },
+});
 </script>
 <style scoped>
-    a {
-      color: #ffffff;
-      font-size: 30px;
-      line-height: 52px;
-      font-weight: bold;
-    }
-    .header-icon {
-        width:42px;
-        height:42px;
-        margin-top: 5px;
-    }
-    .avatar {
-        height: 52px;
-    }
-    .avatar helper{
-        display: inline-block;
-        height: 100%;
-        vertical-align: middle;
-    }
-    .avatar img {
-        width:42px;
-        height:42px;
-        margin-top: 5px;
-        border-radius: 50%;
-    }
-  .el-row {
-    margin-bottom: 20px;
-  }
-  .el-col {
-    border-radius: 0px;
-  }
-  .bg-purple-dark {
-    background: #155263;
-  }
-  .bg-purple {
-    background: #155263;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    min-height: 52px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
+a {
+  color: #ffffff;
+  font-size: 30px;
+  line-height: 52px;
+  font-weight: bold;
+}
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409eff;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+.header-icon {
+  width: 42px;
+  height: 42px;
+  margin-top: 5px;
+}
+.avatar {
+  height: 52px;
+}
+.avatar helper {
+  display: inline-block;
+  height: 100%;
+  vertical-align: middle;
+}
+.avatar img {
+  width: 42px;
+  height: 42px;
+  margin-top: 5px;
+  border-radius: 50%;
+}
+.el-row {
+  margin-bottom: 20px;
+}
+.el-col {
+  border-radius: 0px;
+}
+.bg-purple-dark {
+  background: #155263;
+}
+.bg-purple {
+  background: #155263;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  min-height: 52px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
--- 221801314_221801321/web/ezsearch/src/views/Home.vue
+++ 221801314_221801321/web/ezsearch/src/views/Home.vue
@@ -1,70 +1,83 @@
 <template>
   <div class="block">
-    <Header/>
+    <Header />
     <el-row :gutter="20">
-      <el-col :span="12" :offset="3"><Carousel/></el-col>
-      <el-col :span="6" :offset="0"><Carousel/></el-col>
+      <el-col :span="12" :offset="3"><Carousel /></el-col>
+      <el-col :span="6" :offset="0"><CarouselV /></el-col>
     </el-row>
-    
-    <el-divider></el-divider>
-
-    <el-row :gutter="0">
-      <el-col :span="14" :push="4">
-        <el-input
-          style="width: 80%"
-          placeholder="搜索论文"
-          prefix-icon="el-icon-search"
-          v-model="input">
-        </el-input>
-      </el-col>
-      <el-col :span="6" :push="1">
-        <el-button type="primary">搜索</el-button>
-      </el-col>
-    </el-row>
+    <div class="divider"></div>
+    <div>
+      <el-row :gutter="0">
+        <el-col :span="5" :offset="0"></el-col>
+        <el-col :span="12" >
+          <el-input
+            id="search-input"
+            placeholder="搜索论文"
+            prefix-icon="el-icon-search"
+            v-model="input"
+          >
+          </el-input>
+        </el-col>
+        <el-col :span="1" :push="0">
+          <el-button type="primary" class="search-btn blue-background" style="height: 60px">搜索</el-button>
+        </el-col>
+        <el-col :span="1" :push="0">
+          <el-button type="primary" class="el-icon-s-operation blue-background" style="margin-left:40px; height: 60px; font-size:32px;"></el-button>
+        </el-col>
+        <el-col :span="5" :offset="0"></el-col>
+        
+      </el-row>
+    </div>
+    <div class="divider"></div>
+    <el-space direction="vertical">
+      <Paper />
+      <Paper />
+      <Paper />
+      <Paper />
+      <Paper />
+    </el-space>
+    <Footer />
   </div>
-  <el-divider></el-divider>
-
-  <el-space direction="vertical">
-    <Paper/>
-    <Paper/>
-    <Paper/>
-    <Paper/>
-    <Paper/>
-  </el-space>
-  <Footer/>
 </template>
 <script>
-import {defineComponent, ref} from 'vue';
-import Header from '@/components/Header.vue';
-import Paper from '@/components/Paper.vue';
-import Carousel from '@/components/Carousel.vue';
+import { defineComponent, ref } from "vue";
+import Header from "@/components/Header.vue";
+import Paper from "@/components/Paper.vue";
+import Carousel from "@/components/Carousel.vue";
+import CarouselV from "@/components/CarouselVertical.vue";
 // import Sidebar from '@/components/Sidebar.vue';
-import Footer from '@/components/Footer.vue';
-export default defineComponent ({
+import Footer from "@/components/Footer.vue";
+export default defineComponent({
   name: "Search",
-  components:{
+  components: {
     Header,
     Paper,
     Carousel,
+    CarouselV,
     // Sidebar,
     Footer,
   },
-  setup() { 
+  setup() {
     const path = require("@/assets/CVPR.jpg");
     const images = [
-      { url: require("@/assets/CVPR.jpg")},
-      { url: require('@/assets/ECCV.png')},
-      { url: require('@/assets/ICCV.png')},
-    ]
-      return {
-        input: ref(''),
-        images,
-        path,
-      }
+      { url: require("@/assets/CVPR.jpg") },
+      { url: require("@/assets/ECCV.png") },
+      { url: require("@/assets/ICCV.png") },
+    ];
+    return {
+      input: ref(""),
+      images,
+      path,
+    };
   },
 });
 </script>
 <style>
+.blue-background {
+  background-color: #155263;
+  border: 0px;
+}
+
 .el-carousel__item h3 {
   color: #475669;
   font-size: 14px;
@@ -79,5 +92,22 @@ export default defineComponent ({
 
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
+}
+
+.divider {
+  height: 80px;
+}
+#search-btn input {
+  height: 60px !important;
+  border: 1px solid rgb(47, 47, 47) !important;
+}
+input {
+  height: 60px !important;
+  border: 1px solid rgb(47, 47, 47) !important;
+}
+.search-btn {
+  width: 100px;
+  height: 60px;
+  
 }
 </style>
