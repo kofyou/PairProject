988ed108d48f4057fc25bcad1291bf4927e48318
Tozzger <997053715@qq.com>
2021-03-28 16:33:17 +0800

    bug fix

--- 041801114&071808114/python/json2mysql.py
+++ 041801114&071808114/python/json2mysql.py
@@ -6,6 +6,7 @@ import pymysql
 db = pymysql.connect(host='####',user='####',password='####',database='####')
 sql = "INSERT INTO posts(year, meeting, title, keywords, abstract, url) VALUES (%s, %s, %s, %s, %s, %s)"
 cursor = db.cursor()
+cnt = 0
 def foo(meeting):
     for file in os.listdir("论文数据/" + meeting):
         with open("论文数据/" + meeting + '/' + file, 'r', encoding='utf-8') as f:
@@ -16,14 +17,20 @@ def foo(meeting):
             url = data["doiLink"]
             year = data["publicationYear"]
             cursor.execute(sql, (year, meeting, title, keywords, abstract, url))
+            global cnt
+            cnt += 1
+            if (cnt % 10 == 0):
+                print(cnt)
 try:
-    foo('CVPR')
-    foo('ICCV')
-
-    for eccv in os.listdir(parent):
+    for eccv in os.listdir("论文数据/ECCV"):
         with open("论文数据/ECCV/" + eccv, 'r', encoding='utf-8') as f:
             data = json.load(f)
             cursor.execute(sql, (data["发布时间"][-4:], "ECCV", data["论文名称"], ','.join(data["关键词"]), data["摘要"], data["原文链接"]))
+            cnt += 1
+            if (cnt % 10 == 0):
+                print(cnt)
+    foo('CVPR')
+    foo('ICCV')
 except Exception as e:
     print(e)
     db.rollback()
