2293aec200bea7cf8ebbb7ac652d9292bb6e2101
xydf <690054845@qq.com>
2021-03-28 15:29:07 +0800

    fix: 修复一些bug

--- 221801334&221801230/PaperCV/src/main/resources/static/js/paper-list.js
+++ 221801334&221801230/PaperCV/src/main/resources/static/js/paper-list.js
@@ -3,48 +3,50 @@ $(document).ready(function () {
     // var httpRoot = "http://120.24.27.29:8080";
     var MAX_COUNT = 50;
 
-    items = [];
-
-    $.ajax({
-        url:httpRoot + `/paper/all`,
-        type:"GET",
-        dataType:"json",
-        async:false,
-        success:function(result){
-            for (var i = 0;i < result.length;i++){
-                if (i >= MAX_COUNT){
-                    break;
+    $("#read-list").click(function (){
+        $("#item-list").empty();
+        var items = [];
+        $.ajax({
+            url:httpRoot + `/paper/` + $("#paperSearch").val(),
+            type:"GET",
+            dataType:"json",
+            async:false,
+            success:function(result){
+                for (var i = 0;i < result.length;i++){
+                    if (i >= MAX_COUNT){
+                        break;
+                    }
+                    var new_item = "    <div class=\"item\" id=\"item" + i + "\">\n" +
+                        "        <div class=\"content\">\n" +
+                        "            <div class=\"img\"><img src=\"img/paper.png\" alt=\"paper\" width=\"270px\" height=\"340px\"></div>\n" +
+                        "            <div class=\"text\">\n" +
+                        "                <p class=\"text-head\">" + result[i].title +
+                        "                </p>\n" +
+                        "                <p style=\"font-weight: bolder; margin: 10px 0;\">Abstract:</p>\n" +
+                        "                <div class=\"text-content\">" + result[i].paperAbstract + "</div>\n" +
+                        "                <p><span style=\"font-weight: bolder;\">Keywords:</span>" + result[i].keyWord + "</p>\n" +
+                        "            </div>\n" +
+                        "        </div>\n" +
+                        "        <div class=\"footer\">\n" +
+                        "            <div><a href=\"" + result[i].url + "\">" + result[i].url + "</a></div>\n" +
+                        "            <div>\n" +
+                        "                <a type=\"submit\" class=\"btn btn-primary mb-2\">翻译</a>\n" +
+                        "                <a type=\"submit\" class=\"btn btn-primary mb-2\">收藏</a>\n" +
+                        "                <a type=\"submit\" name=\"item" + i + "\" class=\"btn btn-primary mb-2" +
+                        " delete-item\">删除</a>\n" +
+                        "            </div>\n" +
+                        "        </div>\n" +
+                        "    </div>"
+                    items.push(new_item);
                 }
-                var new_item = "    <div class=\"item\" id=\"item" + i + "\">\n" +
-                    "        <div class=\"content\">\n" +
-                    "            <div class=\"img\"><img src=\"img/paper.png\" alt=\"paper\" width=\"270px\" height=\"340px\"></div>\n" +
-                    "            <div class=\"text\">\n" +
-                    "                <p class=\"text-head\">" + result[i].title +
-                    "                </p>\n" +
-                    "                <p style=\"font-weight: bolder; margin: 10px 0;\">Abstract:</p>\n" +
-                    "                <div class=\"text-content\">" + result[i].paperAbstract + "</div>\n" +
-                    "                <p><span style=\"font-weight: bolder;\">Keywords:</span>" + result[i].keyWord + "</p>\n" +
-                    "            </div>\n" +
-                    "        </div>\n" +
-                    "        <div class=\"footer\">\n" +
-                    "            <div><a href=\"" + result[i].url + "\">" + result[i].url + "</a></div>\n" +
-                    "            <div>\n" +
-                    "                <a type=\"submit\" class=\"btn btn-primary mb-2\">翻译</a>\n" +
-                    "                <a type=\"submit\" class=\"btn btn-primary mb-2\">收藏</a>\n" +
-                    "                <a type=\"submit\" name=\"item" + i + "\" class=\"btn btn-primary mb-2" +
-                    " delete-item\">删除</a>\n" +
-                    "            </div>\n" +
-                    "        </div>\n" +
-                    "    </div>"
-                items.push(new_item);
-            }
-        },
+            },
+        });
+
+        for (var i = 0;i < items.length;i++) {
+            $("#item-list").append(items[i]);
+        }
     });
 
-    $("#first-item").remove();
-    for (var i = 0;i < items.length;i++) {
-        $("#item-list").append(items[i]);
-    }
 
     $(".delete-item").click(function () {
         var id = $(this).attr("name")
