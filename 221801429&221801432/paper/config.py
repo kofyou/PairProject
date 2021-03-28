9962e2eabdc7dfe96617bc7bbc65bb17ca2798a7
koopi0123 <1034088383@qq.com>
2021-03-28 14:34:05 +0800

    主页显示

---
+++ 221801429&221801432/paper/config.py
@@ -0,0 +1,10 @@
+"""
+  相关配置信息
+"""
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://%s:%s@%s/%s" % ('root', 'HRYYCFLqswslhk1', '127.0.0.1', 'paper')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+    # REDIS_HOST = "127.0.0.1"
+    # REDIS_PORT = 6379
