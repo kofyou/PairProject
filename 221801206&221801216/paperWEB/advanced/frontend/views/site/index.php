17ac8a5bbb0597b34ea5700b8c5ada9cbe3a94c4
lenovo <1805456146@qq.com>
2021-03-28 21:38:31 +0800

    Create index.php
    Create index.php

---
+++ 221801206&221801216/paperWEB/advanced/frontend/views/site/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Paper Garden';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Welcome to PaperWEB</h1>
+
+ 
+        
+
+    </div>
+</div>
