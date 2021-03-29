c218bca1a1a69ddf1a059cec47d178a0164c6e2c
wuhan <1147306550@qq.com>
2021-03-29 15:40:15 +0800

    实现注册功能--后端

--- 221801118_221801136/backend/user/user.go
+++ 221801118_221801136/backend/user/user.go
@@ -32,7 +32,25 @@ func UserLogin(c *gin.Context){
 
 // UserRegister 用户注册
 func UserRegister(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
 
+	rows, _ := database.DB.Raw("select * from user where username = ? ", username).Rows()
+	var i int
+	if rows.Next() {
+		i++
+	}
+
+	if (i == 0 && len(username) <= 60 && len(password) >= 6) {
+		affected := database.DB.Exec("insert into user values(?, ?)", username, password).RowsAffected
+		if(affected == 1){
+			c.JSON(http.StatusOK, gin.H{"ifSucceed":true})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"ifSucceed":false})
+		}
+	} else {
+		c.JSON(http.StatusOK, gin.H{"ifSucceed":false})
+	}
 }
 
 
