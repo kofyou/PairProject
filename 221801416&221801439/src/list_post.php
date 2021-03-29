a0e252adf97499ea197ac8b8bc9d0343f7874778
lulili666 <79951049+lulili666@users.noreply.github.com>
2021-03-29 16:58:48 +0800

    用户文章列表页面前端

--- 221801416&221801439/src/list_post.php
+++ 221801416&221801439/src/list_post.php
@@ -3,19 +3,21 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
   <head>
     <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
-    <title>Simple Blog System—发表文章</title>
+    <link rel="stylesheet" href="list_post.css" type="text/css" />
+    <title>发表文章</title>
   </head>
   <body>
-  <h1>Simple Blog System—文章列表</h1>
+  <h1 id="title">文章列表</h1>
+  <div class="operation">
   <a href="new_post.html">新增文章</a>
   <a href="data.php">关键词图谱与热词走势</a>
+
   <br/>
   <br/>
   <form action = "checklist.php" method="post">
-    <input type="text" name="title" value="请输入标题关键字进行搜索"  onfocus='if(this.value=="请输入标题关键字进行搜索"){this.value="";}; '   
+    <input type="text" name="title" value="请输入标题关键字进行搜索"  id="key" onfocus='if(this.value=="请输入标题关键字进行搜索"){this.value="";}; '   
             onblur='if(this.value==""){this.value="请输入标题关键字进行搜索";};'/>
-    
-    <select name="time">
+    <select name="time" id="time">
       <option value="所有时间">选择会议时间</option>
       <option value="2007">2007</option>
       <option value="2008">2008</option>
@@ -31,6 +33,22 @@
       <option value="2018">2018</option>
       <option value="2019">2019</option>
     </select>
+    <!-- <input id="type" type="text" list="typelist" placeholder="选择会议时间">
+        <datalist id="typelist">
+  　　    <option>2007</option>
+  　　    <option>2008</option>
+          <option>2009</option>
+          <option>2010</option>
+          <option>2011</option>
+  　　    <option>2012</option>
+          <option>2013</option>
+          <option>2014</option>
+          <option>2015</option>
+  　　    <option>2016</option>
+          <option>2017</option>
+          <option>2018</option>
+          <option>2019</option>
+        </datalist> -->
     <input type="submit" value="搜索"/>
     <select name="sort">选择排序方式
       <option value="post_title">标题</option>
@@ -41,6 +59,7 @@
   <form action="loginout.php" method="post">
     <input type="submit" value="退出登录"/>
   </form>
+</div>
 <?php
   require('config.php');
 
@@ -58,18 +77,18 @@
 
   $num_results = $result->num_rows;
 
-  echo '<p>共有文章: '.$num_results.' 篇</p>';
+  echo '<p id="num">共有文章: '.$num_results.' 篇</p>';
 
   for ($i=0; $i <$num_results; $i++)
   {
      $row = $result->fetch_assoc();
-     echo '<h2>'.($i+1).'.  ' . htmlspecialchars(stripslashes($row['post_title'])) . "</h2>\n";
-     echo '<p>发表时间：' . htmlspecialchars(stripslashes($row['release_date'])) . "</p>\n";
-     echo '<p>会议时间：' . htmlspecialchars(stripslashes($row['meeting_date'])) . "</p>\n";
-     echo '<p>操作：'.'<a href="update_post.php?title='.htmlspecialchars(stripslashes($row['post_title'])).'">修改</a>';
-     echo '  '.'<a href="delete_post.php?title='.htmlspecialchars(stripslashes($row['post_title'])).'">删除</a>';
-     echo '<p>摘要：'.nl2br(htmlspecialchars(stripslashes($row['post_content']))).'</p>';
-     echo '<a href="'.htmlspecialchars(stripslashes($row['link'])).'">原文链接</a>';
+     echo '<div class="list"><h2>'.($i+1).'.  ' . htmlspecialchars(stripslashes($row['post_title'])) . "</h2>\n".
+     '<p>发表时间：' . htmlspecialchars(stripslashes($row['release_date'])) . "</p>\n".
+     '<p>会议时间：' . htmlspecialchars(stripslashes($row['meeting_date'])) . "</p>\n".
+     '<p>操作：'.'<a href="update_post.php?title='.htmlspecialchars(stripslashes($row['post_title'])).'">修改</a>'.
+     '  '.'<a href="delete_post.php?title='.htmlspecialchars(stripslashes($row['post_title'])).'">删除</a>'.
+     '<p>摘要：'.nl2br(htmlspecialchars(stripslashes($row['post_content']))).'</p>'.
+     '<a href="'.htmlspecialchars(stripslashes($row['link'])).'"><button  id="link">原文链接</button></a></div>';
   }
 
   $result->free();
