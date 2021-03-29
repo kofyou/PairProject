7b8ffecaff6c963dbe23c247dc19174d0dc311d7
2689124501 <2689124501@qq.com>
2021-03-30 00:09:59 +0800

    建立项目
    1. 创建Springboot项目 添加配置文件 创建项目目录结构

---
+++ 221801310&221801325/backend/src/main/java/com/pair/papercrawler/PapercrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.pair.papercrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapercrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapercrawlerApplication.class, args);
+    }
+
+}
---
+++ 221801310&221801325/backend/src/test/java/com/pair/papercrawler/PapercrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pair.papercrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapercrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
