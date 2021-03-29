f900bad8a189467ab0839de2acba7ee35c0e009b
lulili666 <79951049+lulili666@users.noreply.github.com>
2021-03-29 22:12:00 +0800

    检查操作页面前端

--- 221801416&221801439/src/checklist.php
+++ 221801416&221801439/src/checklist.php
@@ -3,17 +3,24 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
   <head>
     <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
-    <title>Simple Blog System—发表文章</title>
+    <link rel="stylesheet" href="checklist.css" type="text/css" />
+    <title id="title">操作结果</title>
   </head>
   <body>
-  <h1>Simple Blog System—文章列表</h1>
-  <a href="new_post.html">新增文章</a>
+  <form action="loginout.php" method="post">
+    <input type="submit" value="退出登录" id="exit"/>
+  </form>
+  <h1 id="title">文章列表</h1>
+  <div class="operation">
+  <a href="new_post.html"><button id="new">新增文章</button></a>
+  </div>
   <br/>
   <br/>
+  <div class="search">
   <form action = "list_post.php" method="post">
-    <input type="text" name="title" value="请输入标题关键字进行搜索"  onfocus='if(this.value=="请输入标题关键字进行搜索"){this.value="";}; '   
+    <input type="text" name="title"  id="key" value="请输入标题关键字进行搜索"  onfocus='if(this.value=="请输入标题关键字进行搜索"){this.value="";}; '   
             onblur='if(this.value==""){this.value="请输入标题关键字进行搜索";};'/>
-    <select name="time">
+    <select name="time" id="time">
       <option value="所有时间">选择会议时间</option>
       <option value="2007">2007</option>
       <option value="2008">2008</option>
@@ -29,16 +36,15 @@
       <option value="2018">2018</option>
       <option value="2019">2019</option>
     </select>
-    <input type="submit" value="搜索"/>
-    <select name="sort">选择排序方式
+    <input type="submit" value="搜索" id="search"/>
+    <br/>
+    <select name="sort" id="sort">选择排序方式
       <option value="post_title">标题</option>
       <option value="meeting_date">年份</option>
     </select>
-    <input type="submit" value="排序"/>
+    <input type="submit" value="排序" id="sbtn"/>
   </form>
-  <form action="loginout.php" method="post">
-    <input type="submit" value="退出登录"/>
-  </form>
+</div>
 <?php
   require('config.php');
 
@@ -50,9 +56,9 @@
   }
     // 设置字符集
   $db->query("SET NAMES utf8");
-  $title = $_POST["title"];
-  $time = $_POST["time"];
-  $sort = $_POST["sort"];
+  $title = isset($_POST["title"])?$_POST["title"]:'NA';
+  $time = isset($_POST["time"])?$_POST["time"]:'NA';
+  $sort = isset($_POST["sort"])?$_POST["sort"]:'NA';
   if($title != "请输入标题关键字进行搜索" && $title != null && $time != "所有时间") {
     $query = "select * from paper where post_title like '%".$title."%' AND meeting_date like '%".$time."' order by ".$sort;
   }
@@ -69,18 +75,18 @@
 
   $num_results = $result->num_rows;
 
-  echo '<p>共有文章: '.$num_results.' 篇</p>';
+  echo '<p id="num">共有文章: '.$num_results.' 篇</p>';
 
   for ($i=0; $i <$num_results; $i++)
   {
      $row = $result->fetch_assoc();
-     echo '<h2>'.($i+1).'.  ' . htmlspecialchars(stripslashes($row['post_title'])) . "</h2>\n";
-     echo '<p>发表时间：' . htmlspecialchars(stripslashes($row['release_date'])) . "</p>\n";
-     echo '<p>会议时间：' . htmlspecialchars(stripslashes($row['meeting_date'])) . "</p>\n";
-     echo '<p>操作：'.'<a href="update_post.php?title='.htmlspecialchars(stripslashes($row['post_title'])).'">修改</a>';
-     echo '  '.'<a href="delete_post.php?title='.htmlspecialchars(stripslashes($row['post_title'])).'">删除</a>';
-     echo '<p>摘要：'.nl2br(htmlspecialchars(stripslashes($row['post_content']))).'</p>';
-     echo '<a href="'.htmlspecialchars(stripslashes($row['link'])).'">原文链接</a>';
+     echo '<div class="list"><h2>'.($i+1).'.  ' . htmlspecialchars(stripslashes($row['post_title'])) . "</h2>\n".
+    '<p>发表时间：' . htmlspecialchars(stripslashes($row['release_date'])) . "</p>\n".
+    '<p>会议时间：' . htmlspecialchars(stripslashes($row['meeting_date'])) . "</p>\n".
+    '<p>操作：'.'<a href="update_post.php?title='.htmlspecialchars(stripslashes($row['post_title'])).'"><button  id="link">修改</button></a>'.
+    '  '.'<a href="delete_post.php?title='.htmlspecialchars(stripslashes($row['post_title'])).'"><button  id="link">删除</button></a>'.
+    '<p>摘要：'.nl2br(htmlspecialchars(stripslashes($row['post_content']))).'</p>'.
+    '<a href="'.htmlspecialchars(stripslashes($row['link'])).'"><button  id="link">原文链接</button></a></div>';
   }
 
   $result->free();
