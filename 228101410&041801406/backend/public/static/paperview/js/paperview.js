df79382b91f372a71ad89e9beb58f972b1acfd13
hzj2870757140 <https://github.com/hzj2870757140>
2021-03-29 16:48:58 +0800

    完成了爬取论文列表的模糊搜索和排序

--- 228101410&041801406/backend/public/static/paperview/js/paperview.js
+++ 228101410&041801406/backend/public/static/paperview/js/paperview.js
@@ -1,69 +1,63 @@
-var tableData = [];
-window.onload = function(){
-    var href=window.location.search;
-    var word = href.substring(href.lastIndexOf('=')+1, href.length);//获取要搜索的数据
-    var wordlist =word.split('+');
-    if(wordlist[1]=="1")
-    {
-        if(wordlist[0]!='')
-        {
-            $.ajax(
-            {
-                url: '../index.php/index/Serchfunction/serchdata?words='+wordlist[0],
+var td = [];
+window.onload = function() {
+    var href = window.location.search;
+    var word = href.substring(href.lastIndexOf('=') + 1, href.length); //获取要搜索的数据
+    var wordlist = word.split('+');
+    if (wordlist[1] == "1") {
+        if (wordlist[0] != '') {
+            $.ajax({
+                url: '../index.php/index/Serchfunction/serchdata?words=' + wordlist[0],
                 type: 'get',
                 data: {},
                 dataType: 'json'
-            }).then(function (res) 
-            {
-                for (var i in res) 
-                {
-                    tableData.push(res[i])  //把从json获取的数据赋值给数组
+            }).then(function(res) {
+                for (var i in res) {
+                    td.push(res[i]) //把从json获取的数据赋值给数组
                 }　　　　　　　　　　
-            }).fail(function () 
-            {
-                    console.log('失败');
+            }).fail(function() {
+                console.log('失败');
             })
         }
-    }
-    else if(wordlist[1]=="2")
-    {
-        if(wordlist[0]!='')
-        {
-            $.ajax(
-            {
-                url: '../index.php/index/Serchfunction/serchbykeyword?words='+wordlist[0],
+    } else if (wordlist[1] == "2") {
+        if (wordlist[0] != '') {
+            $.ajax({
+                url: '../index.php/index/Serchfunction/serchbykeyword?words=' + wordlist[0],
                 type: 'get',
                 data: {},
                 dataType: 'json'
-            }).then(function (res) 
-            {
-                for (var i in res) 
-                {
-                    tableData.push(res[i])  //把从json获取的数据赋值给数组
+            }).then(function(res) {
+                for (var i in res) {
+                    tableData.push(res[i]) //把从json获取的数据赋值给数组
                 }　　　　　　　　　　
-            }).fail(function () 
-            {
-                    console.log('失败');
+            }).fail(function() {
+                console.log('失败');
             })
         }
     }
-    
+
 }
 var Main = {
     data() {
         return {
-            tableData,
-            currentPage1:1,
-            pagesize:10,
-            currpage:1,
+            tableData: td,
+            sourceData: td,
+            currentPage1: 1,
+            pagesize: 10,
+            currpage: 1,
+            devfilter: '',
         }
     },
-    methods: {
-        handleSizeChange(val){
-            this.pagesize=val;
+    watch: {
+        devfilter: function(val, oldval) {
+            this.tableData = this.sourceData.filter(item => (~item.title.indexOf(val)));
         },
-        handleCurrentChange(val){
-            this.currpage=val;
+    },
+    methods: {
+        handleSizeChange(val) {
+            this.pagesize = val;
+        },
+        handleCurrentChange(val) {
+            this.currpage = val;
         },
         handleClick(row) {
             tableData.splice(tableData.findIndex(e => e.title == row.title), 1);
@@ -74,11 +68,27 @@ var Main = {
             var sortingType = column.order;
             //按照降序排序
             if (sortingType == "descending") {
-                this.tableData = this.tableData.sort((a, b) => Date.parse(new Date(b[fieldName])) - Date.parse(new Date(a[fieldName])));
+                this.tableData = this.tableData.sort(function(a, b) {
+                    a = a[fieldName];
+                    b = b[fieldName];
+                    if (a.length == b.length) {
+                        return b.localeCompare(a);
+                    } else {
+                        return b.length - a.length;
+                    }
+                });
             }
             //按照升序排序
             else {
-                this.tableData = this.tableData.sort((a, b) => Date.parse(new Date(a[fieldName])) - Date.parse(new Date(b[fieldName])));
+                this.tableData = this.tableData.sort(function(a, b) {
+                    a = a[fieldName];
+                    b = b[fieldName];
+                    if (a.length == b.length) {
+                        return a.localeCompare(b);
+                    } else {
+                        return a.length - b.length;
+                    }
+                });
             }
         }
     },
