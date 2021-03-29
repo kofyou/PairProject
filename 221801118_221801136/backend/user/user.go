516a3b6f6f681eb4027724d0c5768749e4b9a551
wuhan <1147306550@qq.com>
2021-03-29 15:37:04 +0800

    实现用户登录--后端

--- 221801118_221801136/backend/user/user.go
+++ 221801118_221801136/backend/user/user.go
@@ -1,16 +1,31 @@
 package user
 
 import (
+	"backend/database"
 	_ "backend/database"
 	_ "database/sql"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	_ "strings"
 )
 
 
 // UserLogin 用户登录
 func UserLogin(c *gin.Context){
+	username := c.Query("username")
+	password := c.Query("password")
 
+	ifExist, _ := database.DB.Raw("select * from user where username = ? and password = ?", username, password).Rows()
+	var i int
+	if ifExist.Next() {
+		i++
+	}
+
+	if (i == 1) {
+		c.JSON(http.StatusOK, gin.H{"ifCorrect":true})
+	}else {
+		c.JSON(http.StatusOK, gin.H{"ifCorrect":false})
+	}
 
 }
 
