f9002510cff0c2ee19bf05b228de72c9f354119b
lvvvv <375042084@qq.com>
2021-03-30 14:43:25 +0800

    [Added] 添加了词云图表，但存在显示bug

---
+++ 221801314_221801321/web/ezsearch/src/components/graph/WordCloud.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="wordCloud" :style="{ width: '300px', height: '300px' }"></div>
+</template>
+
+
+<script>
+import {
+  inject,
+  onMounted,
+  mounted,
+  defineComponent,
+  getCurrentInstance,
+} from "vue";
+export default defineComponent({
+  name: "WordCloud",
+  setup() {
+    let echarts = inject("ec"); //引入
+    onMounted(() => {
+      let myChart = echarts.init(document.getElementById("wordCloud"));
+      // 绘制图表
+    });
+    return {
+    };
+  },
+});
+</script>
+<style scoped>
+</style>
