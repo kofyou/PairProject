6240b7ae8bd23c40d6dab3fa50dcd1a23b8e4378
lvvvv <375042084@qq.com>
2021-03-30 16:32:50 +0800

    [Updated] 删除了部分注释和词云入口

--- 221801314_221801321/web/ezsearch/src/views/Graph.vue
+++ 221801314_221801321/web/ezsearch/src/views/Graph.vue
@@ -2,7 +2,7 @@
   <Header />
   <div>
       <router-link to="/hotspot"><span style="font-size: 20px">HotSpot</span></router-link><span style="font-size: 20px"> | </span>
-      <router-link to="/wordCloud"><span style="font-size: 20px">HotSpot</span></router-link><span style="font-size: 20px"> | </span>
+      <!-- <router-link to="/wordCloud"><span style="font-size: 20px">HotSpot</span></router-link><span style="font-size: 20px"> | </span> -->
       <router-link to="/top10"><span style="font-size: 20px">Top10</span></router-link>
 
       <router-view style="margin: 0 auto; margin-top:60px"></router-view>
