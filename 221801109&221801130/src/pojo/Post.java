e4260d2ecb7e50b1895c4588f4f92100299e2ca1
liangyangxin <965720407@qq.com>
2021-03-30 20:03:28 +0800

    更新post类

--- 221801109&221801130/src/pojo/Post.java
+++ 221801109&221801130/src/pojo/Post.java
@@ -65,23 +65,5 @@ public class Post {
 	public void setPlatform(String platform) {
 		this.platform = platform;
 	}
-	/*public String getAnonymousName() {
-        if (null == name)
-            return null;
-
-        if (name.length() <= 1)
-            return "*";
-
-        if (name.length() == 2)
-            return name.substring(0, 1) + "*";
-
-        char[] categories = name.toCharArray();
-        for (int i = 1; i < categories.length - 1; i++) {
-            categories[i] = '*';
-        }
-        return new String(categories);
-
-
-    }*/
 
 }
