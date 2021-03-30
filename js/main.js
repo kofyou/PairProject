153e7e13aaeefcda20ad8fd643e42355e2dd186d
yangyi16123 <1612350727@qq.com>
2021-03-30 19:03:51 +0800

    完成首页参数传递
    完成首页参数传递

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 var app= new Vue({
     el: '#app',
     data: {
-        info
-
+      info:[ ],
+     value:"",
+      type:"keyword"
     },
     methods:{
       search:function(){                
+        
             axios
               .get('http://47.100.89.20:8080/homepage/agenda')
-              .then(response => (console(response)))
+              .then(response => (console.log(response) ))
               .catch(function (error) { // 请求失败处理
                 console.log(error);
               });
-              }
+              },
+              to:function(){             
+                console.log(this.value)   
+                console.log(this.type)   
+                window.location.assign("论文列表.html?vaule="+this.value+"&type="+this.type)
+                  }
+              
           }
   })
