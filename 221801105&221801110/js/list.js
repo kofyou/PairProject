207395d22e75203f4613e513dd02baa734ff07c5
sanluo-huiyi <79392286+sanluo-huiyi@users.noreply.github.com>
2021-03-30 16:24:40 +0800

    增加删除按钮的实现功能

---
+++ 221801105&221801110/js/list.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+	var tr=document.getElementsByTagName("tr");
+	for (var i= 0;i < tr.length;i ++){
+		bgcChange(tr[i]);
+	}           
+}
+
+//鼠标悬停事件
+function bgcChange(obj){
+	obj.onmouseover=function(){
+		obj.style.backgroundColor="#CCC";
+	}
+	obj.onmouseout=function(){
+		obj.style.backgroundColor="#fff";
+	}
+}
+
+//创建删除函数
+function remove(obj){
+	var hobby=obj.parentNode.parentNode;
+	hobby.parentNode.removeChild(hobby);
+}
