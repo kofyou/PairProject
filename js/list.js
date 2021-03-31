08864798646077de04dde6a64866409bb7493ca3
yangyi16123 <1612350727@qq.com>
2021-03-31 18:58:51 +0800

    完成分页
    完成分页

--- js/list.js
+++ js/list.js
@@ -8,6 +8,7 @@
 
       ],
       link:" ",
+      pageNum:1,
       word:
       { 
         searchType:"title",
@@ -15,7 +16,6 @@
       }
     },
     created: function (variable) {
-        // `this` 指向 vm 实例      
         query = window.location.search.substring(1);
         vars = query.split("&");
         for (i=0;i<vars.length;i++) {
@@ -30,25 +30,51 @@
                    }
 
         }
-        return(false); 
-        },
-    methods:{
-      search:function(){   
         var that=this;
         axios.post('http://81.68.149.69:18902/search/searchPaper',{
             searchType:this.type,
-            content:this.value
+            content:this.value,
+            pageNum:1,
+            pageSize:5
+        })
+        .then(function(response)
+        {   
+            that.item=response.data.data.list;
+            console.log(that.item.data)
+        }
+        )
+        .catch(function (error) { // 请求失败处理
+          console.log(error);
+        });
+        return(false); 
+        },
+
+    methods:{
+      search:function(pageNum){   
+        var that=this;
+        axios.post('http://81.68.149.69:18902/search/searchPaper',{
+            searchType:this.type,
+            content:this.value,
+            pageNum:this.pageNum,
+            pageSize:5
         })
         .then(function(response)
         {   console.log(response)
-            that.item=response.data.data;
-            console.log(that.item)
+            that.item=response.data.data.list;
+            scrollTo(0,0);
+            
         }
         )
         .catch(function (error) { // 请求失败处理
           console.log(error);
         });
        
+          },
+          Next:function()
+          {
+            this.pagNume=this.pagNume+1,
+            console.log(this.pagNume)
+           this.search()
           }
   },}) 
 
